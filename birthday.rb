71d614af07857c95f1ceae4d50de1f5a97216b9b
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-14 13:39:30 +0000

    added erb files with form layout and updated spec_helper

---
+++ birthday.rb
@@ -0,0 +1,24 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+
+  get '/'do
+    erb :index
+  end
+
+  post '/details' do
+    session[:name] = params[:name]
+    session[:day] = params[:day]
+    session[:month] = params[:month]
+    redirect to('/greetings')
+  end
+
+  get '/greetings' do
+    @name = session[:name]
+    @day = session[:day]
+    @month = session[:month]
+    erb :greetings
+  end
+
+run! if app_file == $0
+end
